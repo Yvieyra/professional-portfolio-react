f03159e38ace34af6dccae976c76d7f9ac685479
Ying Yang <vieyrayessenia8@gmail.com>
2023-03-26 17:13:41 -0600

    added content to aboutme home page

---
+++ my-app/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+export default function AboutMe() {
+    return (
+        <div className="About">
+            <h1>
+                About Me
+            </h1>
+            <img src="/my-app/src/images/new yessenia.png" alt="" width="50" height="48" />
+            <p>I am currently a student at the University of Utah, and I am learning fullstack development. I appreciate the problem solving skills I have learned to develop apps,
+                programs, and just about anything I want with dedication and continuous learning.
+
+                I am a passionate about learning new ideas, skills, and technology who works well with others in a team setting.
+                I have strong communication/emotional intelligence skills. Equally, I work well on my own and have become quite the multi-tasker, I manage my time well.</p>
+        </div>
+    );
+}
