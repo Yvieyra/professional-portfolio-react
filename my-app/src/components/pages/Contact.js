367c9726333362e329ecb843b914dea35a8f5471
Ying Yang <vieyrayessenia8@gmail.com>
2023-04-03 15:34:50 -0600

    edited colors and resoled css for Portfolio Projects page

--- my-app/src/components/pages/Contact.js
+++ my-app/src/components/pages/Contact.js
@@ -1,19 +1,31 @@
 import React from 'react';
-
+import { useForm } from 'react-hook-form';
 
 export default function Contact() {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors }
+    } = useForm();
+    const onSubmit = (values) => alert(JSON.stringify(values, null, 2));
+  
     return (
         <div className="container">
-            <form className='contact-form'>
-                <label htmlFor='fname'>Full Name:</label>
-                <input type='text' placeholder='Full first name here'></input>
-                <label htmlFor='email'>Email Address:</label>
-                <input type='text' placeholder='Email address here'></input>
-                <p class="error">Please enter a valid email address</p>
-                <label htmlFor='message'>Message:</label>
-                <textarea type='text' placeholder='Type your message here...' rows='10' cols='60'></textarea>
-                <input type="submit" value="Submit" ></input>
-            </form>
-        </div>    
-    )
-}
+      <form className='contact-form' onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor='fname'>Full Name:</label>
+        <input type='text' placeholder='Full first name here...'></input>
+        <br></br>
+        <input
+          type="text"
+          placeholder="Email address here..."
+          {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })}
+        />
+         <label htmlFor='message'>Message:</label>
+                 <textarea type='text' placeholder='Type your message here...' rows='10' cols='60'></textarea>
+        {errors.email && <span>Invalid email</span>}
+  
+        <input type="submit" />
+      </form>
+       </div>
+    );
+  }
--- my-app/src/components/pages/Portfolio.js
+++ my-app/src/components/pages/Portfolio.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import marvelImage from "../../images/marvel.png"
 import daycareImage from "../../images/daycare.png"
+import insomniaImage from "../../images/insomnia.png"
 
 export default function Portfolio() {
     return (
-        <div className="container">
+        
+        <div className='project-title'>
             <h2>
                 Portfolio Projects
             </h2>
-
-            <div className='marvel'>
+        <div className="portfolio-container">
+            <div className='project-container'>
                 <a href="https://yvieyra.github.io/project-1/" title="Marvel Superheros Webpage" target="_blank" rel="noopener noreferrer">
                     <img src={marvelImage} alt="" width="300" height="180" />
                 </a>
@@ -19,7 +21,7 @@ export default function Portfolio() {
 
             <br></br>
 
-            <div className='daycare'>
+            <div className='project-container'>
                 <a href="https://gentle-fjord-00735.herokuapp.com/login" title="Doggy Daycare Webpage" target="_blank" rel="noopener noreferrer">
                     <img src={daycareImage} alt="" width="300" height="180" />
                 </a>
@@ -28,16 +30,15 @@ export default function Portfolio() {
             </div>
 
             <br></br>
-            
-            <div className='daycare'>
-                <a href="https://gentle-fjord-00735.herokuapp.com/login" title="Doggy Daycare Webpage" target="_blank" rel="noopener noreferrer">
-                    <img src={daycareImage} alt="" width="300" height="180" />
+
+            <div className='project-container'>
+                <a href="https://drive.google.com/file/d/1kGCvRyv6zO0QvLqn5kr6rn9A7lawEP4H/view" title="Media-Api-Nosql" target="_blank" rel="noopener noreferrer">
+                    <img src={insomniaImage} alt="" width="300" height="180" />
                 </a>
                 <br></br>
-                <a href="https://github.com/hannahedwards/dog-daycare.git" target="_blank" rel="noopener noreferrer">github URL</a>
+                <a href="https://github.com/Yvieyra/media-api-nosql.git" target="_blank" rel="noopener noreferrer">github URL</a>
             </div>
-
-
+            </div> 
         </div>
     )
 }
