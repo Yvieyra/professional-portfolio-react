d55eef69c4c2ddc97c51b9082c2571acb90a4cb2
Ying Yang <vieyrayessenia8@gmail.com>
2023-04-09 20:59:31 -0600

    edited onclick function

--- my-app/src/components/pages/Resume.js
+++ my-app/src/components/pages/Resume.js
@@ -1,15 +1,14 @@
 import React from 'react';
-
+import file from '../../Files/Resume.pdf'
 
 export default function Resume() {
     // Function will execute on click of button
     const onButtonClick = () => {
         // using Java Script method to get PDF file
-        fetch('Resume.pdf').then(response => {
+        fetch(file).then(response => {
             response.blob().then(blob => {
                 // Creating new object of PDF file
                 const fileURL = window.URL.createObjectURL(blob);
-                // Setting various property values
                 let alink = document.createElement('a');
                 alink.href = fileURL;
                 alink.download = 'Resume.pdf';
