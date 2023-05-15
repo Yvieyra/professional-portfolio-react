19e0d439cba31fc203780f7d3d905baf94c7bd23
Ying Yang <vieyrayessenia8@gmail.com>
2023-05-15 00:04:18 -0600

    added emamil feature with required fields code

--- my-app/src/components/pages/Contact.js
+++ my-app/src/components/pages/Contact.js
@@ -1,34 +1,39 @@
-import React from 'react';
+
+import React, { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useForm();
-  const onSubmit = (values) => alert(JSON.stringify(values, null, 2));
+
+  const form = useRef();
 
   const sendEmail = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     alert("submitted");
-    emailjs.sendForm("service_gw56ujl", "template_1atz8br", e.target, 'flKK_27oTKr0cb8jg')
+    // onSubmit();
+    emailjs.sendForm("service_gw56ujl", "template_1atz8br", form.current, 'flKK_27oTKr0cb8jg');
+    const onSubmit = (values) => alert(JSON.stringify(values, null, 2));
   }
 
   return (
     <div className="container">
-      <form className='contact-form' onSubmit={handleSubmit(onSubmit) && (sendEmail)}>
+      <form className='contact-form' ref={form} onSubmit={handleSubmit(sendEmail)}>
         <label htmlFor='fname'>Full Name:</label>
-        <input type='text' placeholder='Full first name here...' {...register('fname', { required: true, minlength: 2 })}></input>
+        <input type='text' placeholder='Full first name here...' {...register('fname', { required: true })}></input>
         {errors.fname && <span>*required field*</span>}
         <br></br>
-        <label htmlFor='email'>Email Address:</label>
+        <label htmlFor='email_from'>Email Address:</label>
         <input
           name="email_from"
           type="text"
           placeholder="Email address here..."
-          {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })}
+          {...register("email_from", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })}
         />
         {errors.email && <span>*Invalid email*</span>}
         <br></br>
