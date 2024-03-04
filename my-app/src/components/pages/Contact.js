99f4dd63c21a186b59f75bd793fd11e83e3d913d
Ying Yang <vieyrayessenia8@gmail.com>
2024-03-03 21:46:47 -0700

    resolved emailjs issue, emails are now sending

--- my-app/src/components/pages/Contact.js
+++ my-app/src/components/pages/Contact.js
@@ -17,16 +17,16 @@ export default function Contact() {
     // e.preventDefault();
     alert("submitted");
     // onSubmit();
-    emailjs.sendForm("service_gw56ujl", "template_1atz8br", form.current, 'flKK_27oTKr0cb8jg');
+    emailjs.sendForm("service_o4g8i0s", "template_cdnlige", form.current, 'n4mpMlKU00RpYVtUE');
     let onSubmit = (values) => alert(JSON.stringify(values, null, 2));
   }
 
   return (
     <div className="container">
       <form className='contact-form' ref={form} onSubmit={handleSubmit(sendEmail)}>
-        <label htmlFor='fname'> Your Full Name:</label>
-        <input type='text' placeholder='Full first name here...' {...register('fname', { required: true })}></input>
-        {errors.fname && <span>*required field*</span>}
+        <label htmlFor='from_name'> Your Full Name:</label>
+        <input type='text' placeholder='Full first name here...' {...register('from_name', { required: true })}></input>
+        {errors.from_name && <span>*required field*</span>}
         <br></br>
         <label htmlFor='email_from'> Your Email Address:</label>
         <input
